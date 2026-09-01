540ff5f57083976682b01df467da97e44399166a
Dev pathak <devpathak9685@gmail.com>
2026-09-02 02:10:52 +0530

    Aligned all the pages properly

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -38,7 +38,7 @@ const ABOUT_CARDS = [
 export default function AboutPage() {
   return (
     <div className="flex flex-col flex-1">
-      <main className="relative flex size-full w-full flex-col items-center justify-start gap-10 px-4 pt-20 pb-0">
+      <main className="relative flex size-full w-full flex-col items-center justify-start gap-10 px-4 pt-32 pb-0">
         <Link href="/" className="absolute top-6 left-6 z-20 md:top-8 md:left-8">
           <img src="/logo.png" alt="Meyvex" className="h-9 w-auto md:h-11" />
         </Link>
